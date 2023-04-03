b9038d09f3a1e210ed71ae636255c90da9351ecc
Muhammed Yahya VARLIK <myvarlik@icloud.com>
2023-04-04 01:44:46 +0300

    bug fixed

--- src/setup/axiosConfig.js
+++ src/setup/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 let apiUrl = 'https://api.uludagenerji.net/web-sitesi/site';
-apiUrl = 'http://localhost:8090/web-sitesi/site';
+//apiUrl = 'http://localhost:8090/web-sitesi/site';
 
 axios.defaults.baseURL = apiUrl;
 axios.defaults.timeout = 60000;
